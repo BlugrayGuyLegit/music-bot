4e25816c29c1f2061e5bedb22bc1bcef96b3d121
Blugray <162154088+BlugrayGuyLegit@users.noreply.github.com>
2024-05-25 21:56:57 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
 import discord
-from discord.ext import commands
-import youtube_dl
 
-# Bot initialization
-bot = commands.Bot(command_prefix='!')
+# Initialize Discord client
+client = discord.Client()
 
-@bot.event
+# URL of the web page to monitor
+url = "https://dafuqboom.shop"
+
+# Variable to store the previous version of the content
+previous_content = None
+
+# Function to send message to Discord channel
+async def send_message(message):
+    channel_id = 1243967560647577710  # Replace with your Discord channel ID
+    channel = client.get_channel(channel_id)
+    await channel.send(message)
+
+# Function to check for updates
+async def check_for_updates():
+    global previous_content
+    while True:
+        # Retrieve the content of the web page
+        response = requests.get(url)
+        current_content = response.text
+
+        # Check if there's a difference between current and previous content
+        if current_content != previous_content:
+            # Send notification to Discord channel
+            await send_message("The web page has been updated!")
+
+            # Update previous_content with the current content
+            previous_content = current_content
+
+        # Wait for a certain amount of time before checking again
+        await asyncio.sleep(60)  # Check every minute
+
+# Event handler for when the bot is ready
+@client.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print('Logged in as {0.user}'.format(client))
+    # Start checking for updates
+    await check_for_updates()
 
-@bot.command()
-async def play(ctx, url):
-    voice_channel = ctx.author.voice.channel
-    if voice_channel is None:
-        await ctx.send("You must be in a voice channel to use this command.")
-    else:
-        await voice_channel.connect()
-        ctx.voice_client.stop()
-        FFMPEG_OPTIONS = {
-            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-            'options': '-vn'
-        }
-        YDL_OPTIONS = {'format': 'bestaudio'}
-        vc = ctx.voice_client
-
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-            vc.play(source)
-
-@bot.command()
-async def leave(ctx):
-    voice_client = ctx.message.guild.voice_client
-    await voice_client.disconnect()
-
-bot.run('DISCORD_BOT_TOKEN')
+# Run the bot
+client.run('YOUR_DISCORD_BOT_TOKEN')  # Replace with your Discord bot token
